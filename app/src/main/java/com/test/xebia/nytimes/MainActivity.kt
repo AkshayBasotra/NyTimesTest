ec842f50b094af12d92bd37a7be7dc9f272fd60e
akshaybasotra <akshaybasotra@gmail.com>
2019-12-05 07:27:39 +0530

    test commit.

--- app/src/main/java/com/test/xebia/nytimes/MainActivity.kt
+++ app/src/main/java/com/test/xebia/nytimes/MainActivity.kt
@@ -2,7 +2,7 @@ package com.test.xebia.nytimes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//test commit
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
